0caddb5eb6eeea065d565ba0a8d12ca07fbd2669
muhammadsenopati <161024283+muhammadsenopati@users.noreply.github.com>
2024-03-15 13:49:59 +0700

    second commit

---
+++ src/src/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    String id;
+    String judul;
+    String penulis;
+    int jumlah;
+
+    public Book (String id, String judul, String penulis, int jumlah){
+        this.id = id;
+        this.judul = judul;
+        this.penulis = penulis;
+        this.jumlah = jumlah;
+    }
+}
---
+++ src/src/User.java
@@ -0,0 +1,13 @@
+public class User {
+    String nim;
+    String nama;
+    String fakultas;
+    String programStudi;
+
+    public User(String nim, String nama, String fakultas, String programStudi){
+        this.nim = nim;
+        this.nama = nama;
+        this.fakultas = fakultas;
+        this.programStudi = programStudi;
+    }
+}
